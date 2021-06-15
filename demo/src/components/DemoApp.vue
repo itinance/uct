5fe7ba2a3a17480bdec287b910078f881320a687
HenrikTrudewind <henrik@blockchain-solutions.tech>
2021-06-15 16:09:49 +0200

    Updated Demo with values from on Chain contract

--- demo/src/components/DemoApp.vue
+++ demo/src/components/DemoApp.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home">
     <b-container fluid>
+      <b-row >
+        <b-col cols="4" offset="2">
+          <b-form-radio-group style="margin: 0 2rem 2rem 0;" v-model="selectedOption" :options="options"></b-form-radio-group>
+        </b-col>
+      </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Gesamt:
         </b-col>
         <b-col cols="3">
@@ -10,7 +15,7 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Datum:
         </b-col>
         <b-col cols="3">
@@ -18,15 +23,15 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Aktueller Anspruch:
         </b-col>
         <b-col cols="3">
-          {{ activatedAmount.toFixed(4) }}
+          {{ (activatedAmount * totalAmount).toFixed(4) }}
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Aktueller Burn
         </b-col>
         <b-col cols="3">
@@ -34,7 +39,7 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Gewünschte Ausschüttung:
         </b-col>
         <b-col cols="3">
@@ -42,15 +47,15 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Tokens an Wallet:
         </b-col>
         <b-col cols="3">
-          {{ (wantedAmount - (wantedAmount * burnAmount)).toFixed(3) }}
+          {{ (wantedAmount * (1 - burnAmount)).toFixed(3) }}
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Tokens Burned:
         </b-col>
         <b-col cols="3">
@@ -58,11 +63,11 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="2">
+        <b-col cols="3">
           Tokens Übrig:
         </b-col>
         <b-col cols="3">
-          {{ (activatedAmount - wantedAmount).toFixed(3) }}
+          {{ ((activatedAmount * totalAmount) - wantedAmount).toFixed(3) }}
         </b-col>
       </b-row>
     </b-container>
@@ -73,6 +78,7 @@
 <script>
 import Datepicker from 'vuejs-datepicker'
 import {en, de} from 'vuejs-datepicker/dist/locale'
+import {ethers} from 'ethers'
 
 export default {
   name: 'DemoApp',
@@ -83,23 +89,45 @@ export default {
       startDate: new Date(2021, 3, 15),
       wantedAmount: 0,
       en: en,
-      de: de
+      de: de,
+      provider: null,
+      options: [
+        {text: 'Javascript', value: 'javascript'},
+        {text: 'Testnet', value: 'testnet'}
+      ],
+      selectedOption: 'javascript',
+      contract: null
     }
   },
   components: {
     Datepicker
   },
-  computed: {
+  asyncComputed:{
     activatedAmount: {
-      get() {
-        return this.getActivatedAmount(this.startDate, this.date) * this.totalAmount;
-      }
+      async get() {
+        let toReturn = 0;
+        if(this.selectedOption === 'javascript'){
+          return this.getActivatedAmount(this.startDate, this.date);
+        } else if (this.selectedOption === 'testnet'){
+          let activated = Number(await this.contract.getActivatedAmount(this.convertDate(this.startDate), this.convertDate(this.date)));
+          toReturn = activated * (10 ** -18);
+        }
+        return Number(toReturn);
+      },
+      default: 0
     },
     burnAmount: {
-      get() {
-
-        return this.getBurnAmount(this.startDate, this.date);
-      }
+      async get () {
+        let toReturn = 0;
+        if(this.selectedOption === 'javascript'){
+          return this.getBurnAmount(this.startDate, this.date);
+        } else if (this.selectedOption === 'testnet'){
+          let burn = Number(await this.contract.getBurnAmount(this.convertDate(this.startDate), this.convertDate(this.date)));
+          toReturn = burn * (10 ** -18);
+        }
+        return Number(toReturn);
+      },
+      default: 0.8
     }
   },
   methods: {
@@ -114,7 +142,21 @@ export default {
       let days = Math.floor(Math.abs((endDate - startDate) / day));
       let burnAmount = 0.8 - (days * (0.8 / (2 * 365)));
       return Math.max(burnAmount, 0);
+    },
+    convertDate(date){
+      return Math.floor(date.getTime() / 1000);
     }
+  },
+  mounted: function() {
+    this.provider = new ethers.providers.EtherscanProvider('rinkeby', process.env.ETHERSCAN_API_KEY);
+    let conAddress = '0x5E255A677341985464FA7855B0CB0D88bc56b42A';
+    let conAbi = [
+      {"inputs":[{"internalType":"uint256","name":"startDate","type":"uint256"},{"internalType":"uint256","name":"endDate","type":"uint256"}],
+        "name":"getActivatedAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},
+      {"inputs":[{"internalType":"uint256","name":"startDate","type":"uint256"},{"internalType":"uint256","name":"endDate","type":"uint256"}],
+        "name":"getBurnAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"}
+    ]
+    this.contract = new ethers.Contract(conAddress, conAbi, this.provider);
   }
 }
 </script>
--- demo/src/main.js
+++ demo/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
+import AsyncComputed from "vue-async-computed";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(AsyncComputed);
 
 Vue.config.productionTip = false;
 
