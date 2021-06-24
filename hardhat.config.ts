f8ac013f2b5091ef4af3ed2029f2b297bf6b812f
Hagen Hübel <hhuebel@itinance.com>
2021-06-24 12:16:36 +0200

    prepared BSC

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,7 @@ import "@nomiclabs/hardhat-etherscan";
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY || '';
 
 const PRIVATE_KEY_MAINNET = process.env.PRIVATE_KEY_MAINNET || '';
 
@@ -24,6 +25,11 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
     compilers: [{ version: "0.8.3", settings: {} }],
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+     }    
   },
   networks: {
     hardhat: {},
@@ -36,14 +42,30 @@ const config: HardhatUserConfig = {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [PRIVATE_KEY_RINKEBY],
     },
+
+    binance_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [PRIVATE_KEY_RINKEBY],
+    },
+    binance_mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [PRIVATE_KEY_RINKEBY],
+    },
+
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
     },
   },
+
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY,
+    //apiKey: ETHERSCAN_API_KEY,
+    apiKey: BSCSCAN_API_KEY,
   },
 };
 
